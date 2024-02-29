d64676d53058f75bb0cddedfb4cceb7db001013d
Hakob <hkeshis911@student.glendale.edu>
2024-02-29 01:09:05 -0800

    Delete ComputerStore directory

--- ComputerStore/Customers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('CreateUser', views.User_Register , name='CreateAccount'),
-    path('UserLogin', views.Login_Auth, name='LoginPage'),
-    path('UserLogout', views.User_Logout, name='LogoutPage'),
-    path('UserForgotPassword', views.User_Forgot_Password , name='ForgotPassword'),
-]
